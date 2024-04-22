43e060a44a27511accecf44f5af8712c4abae877
Jackson Gray <nohaxjustdoge@gmail.com>
2024-04-22 22:37:19 +0000

    difficult to remember the syntax. took more notes hopefully it comes to me better in the future

--- server/models.py
+++ server/models.py
@@ -3,7 +3,6 @@ from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
-
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
@@ -11,22 +10,41 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    serialize_rules = ("-reviews.customer",)
+    reviews = db.relationship('Review', back_populates="customer")
+    items = association_proxy('reviews', 'item')
 
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
-    __tablename__ = 'items'
+class Item(db.Model, SerializerMixin):
+    __tablename__ = "items"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    serialize_rules = ("-reviews.item",)
+
+    reviews = db.relationship("Review", back_populates="item")
+
     def __repr__(self):
-        return f'<Item {self.id}, {self.name}, {self.price}>'
+        return f"<Item {self.id}, {self.name}, {self.price}>"
+
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    customer = db.relationship('Customer', back_populates="reviews")
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    serialize_rules = ("-customer.reviews", "-item.reviews")
+
+    item = db.relationship('Item', back_populates="reviews")
